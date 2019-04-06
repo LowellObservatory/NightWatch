dcd832383d76bd3728551d8fdad0897c81ed9d83
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-04-06 01:09:04 -0700

    MUCH better specifying basename with wildcards
    No more obnoxious list, but I'll keep it commented for now

--- nightwatch/static/js/hancon.js
+++ nightwatch/static/js/hancon.js
@@ -4,12 +4,16 @@ var hancon = {
     auto_refresh : "1",
 
     image_base : "http://nightwatch.lowell.edu/goes16/nows/",
-    filenames: "goes_latest_023.png, goes_latest_024.png, goes_latest_025.png, goes_latest_026.png, goes_latest_027.png, goes_latest_028.png, goes_latest_029.png, goes_latest_030.png, goes_latest_031.png, goes_latest_032.png, goes_latest_033.png, goes_latest_034.png, goes_latest_035.png, goes_latest_036.png, goes_latest_037.png, goes_latest_038.png, goes_latest_039.png, goes_latest_040.png, goes_latest_041.png, goes_latest_042.png, goes_latest_043.png, goes_latest_044.png, goes_latest_045.png, goes_latest_046.png, goes_latest_047.png",
+    basename : "goes_latest_???.png",
+    num_frames : "24",
+    base_starting_number: "23",
+
+    // filenames: "goes_latest_023.png, goes_latest_024.png, goes_latest_025.png, goes_latest_026.png, goes_latest_027.png, goes_latest_028.png, goes_latest_029.png, goes_latest_030.png, goes_latest_031.png, goes_latest_032.png, goes_latest_033.png, goes_latest_034.png, goes_latest_035.png, goes_latest_036.png, goes_latest_037.png, goes_latest_038.png, goes_latest_039.png, goes_latest_040.png, goes_latest_041.png, goes_latest_042.png, goes_latest_043.png, goes_latest_044.png, goes_latest_045.png, goes_latest_046.png, goes_latest_047.png",
     // NOTE: For background images that are changing on the server, but
     //   have the same filename, you may specify the parameter
     background_static : "n",
 
-    controls : "startstop,speed,step,refresh,toggle",
+    controls : "startstop,speed,step,refresh,looprock,toggle",
     controls_style : "padding:5px;background-color:green;",
     // controls_tooltip : "Start/Stop the animation, Step one frame, Refresh images",
   };
