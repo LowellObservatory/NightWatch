6727bc763c5ecbf58025e1ff31eb084d8acaccaa
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-04-17 11:46:48 -0700

    A mostly working multi-group filtering example!
    Tons of comments too. Still takes two button clicks to work though...

--- nightwatch/static/js/index.js
+++ nightwatch/static/js/index.js
@@ -1,19 +1,51 @@
 // init Isotope
 var $grid = $('.grid').isotope({
-    itemSelector: '.element-item',
-    layoutMode: 'masonry',
-    masonry: {
-      columnWidth: 50,
-    },
+  // disables initial layout
+  isInitLayout: false,
+
+  itemSelector: '.element-item',
+
+  layoutMode: 'masonry',
+  masonry: {
+    columnWidth: 50,
+  },
+
+  // If filter is set to a function, that function checks each
+  //   item (specified by itemSelector) and returns
+  //   true or false if the item should be shown or hidden.
+  filter: filtFunc,
 });
 
-// bind filter button click
+function filtFunc() {
+  // Find the active filter
+  var $activeButt;
+  $('.btn-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $activeButt = $buttonGroup.find('.active').attr('data-filter');
+  });
+
+  // Loop over each relevant element and see if it matches the active filter
+  const groups = JSON.parse($(this).attr('data-groups'));
+  const isElementInCurrentGroup = groups.indexOf($activeButt) !== -1;
+  // Only search elements in the current group
+  if (!isElementInCurrentGroup) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// jQuery loop over all elements that have the 'filters-group' class.
+//   This binds a click on a 'button' inside that group to an actual call
+//   to Isotope to filter based on the value/group specified in the 'button'
+//   that was clicked on, via the "data-filter" attribute on the button itself
 $('.filters-group').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
+  $grid.isotope();
 });
 
-// change is-checked class on buttons
+// jQuery loop over all elements inside the 'btn-group' class.
+//   This will change the "active" class on the button that was clicked on
+//   and remove it from all the rest to show indication for navigation.
 $('.btn-group').each( function( i, buttonGroup ) {
   var $buttonGroup = $( buttonGroup );
   $buttonGroup.on( 'click', 'button', function() {
@@ -22,14 +54,7 @@ $('.btn-group').each( function( i, buttonGroup ) {
   });
 });
 
-// For the first run thru, sort by the default/home/active button
-//   ... but first we have to find which one that is.
-var $activeButt;
-
-$('.btn-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $activeButt = $buttonGroup.find('.active');
-});
-
-var filterValue = $activeButt.attr('data-filter');
-$grid.isotope({ filter: filterValue });
+// Calling isotope() with no arguments triggers arrange(),
+//   which applies both filtering and sorting.
+//   isotope('layout') does neither of those, but can handle size changes.
+$grid.isotope();
