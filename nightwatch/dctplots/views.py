7735441716a0daad3272b0a746ef2410901db5d3
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-05-10 12:13:38 -0700

    Add in DCT Winds, reorganize other groupings

--- nightwatch/dctplots/views.py
+++ nightwatch/dctplots/views.py
@@ -15,28 +15,12 @@ def index(request):
     It's defined in the urlpatterns list in urls.py
     """
 
-    # p = plotWeather.readTempData()
-    # script, div = components(p)
-
-    # p2 = plotWeather.readHumidity()
-    # script2, div2 = components(p2)
-
-    # script4 = server_document("http://localhost:5006/ohlc")
-
-    # script3 = server_document("http://localhost:5006/newact")
-
-    # print(script3)
-    # print(script4)
-
-    # return render(request, 'index.html',
-    #               {'script': script,
-    #                'div': div,
-    #                'script2': script2,
-    #                'div2': div2,
-    #                'script3': script3,
-    #                'script4': script4})
+    # Open question - is just pulling via server_document good here,
+    #   or should I be doing something else?
 
     dctweather = server_document("http://dctsleeperservice:5000/dctweather")
+
+    dctwind = server_document("http://dctsleeperservice:5000/dctwind")
 
     dctinstruments = server_document("http://dctsleeperservice:5000/lmitemps")
 
@@ -46,6 +30,7 @@ def index(request):
 
     return render(request, 'dctplots/index.html',
                   {'dctweatherplot': dctweather,
+                   'dctwind': dctwind,
                    'dctinstrumentsplot': dctinstruments,
                    'facsum_tcs': facsum_tcs,
                    'facsum_lpi': facsum_lpi})
