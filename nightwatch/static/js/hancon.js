e9a02246b5c85902c27c6cc6fcf7200b23ec39d9
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-05-10 12:22:54 -0700

    Update HAniS config values
    Change to ones I think will work well in production

--- nightwatch/static/js/hancon.js
+++ nightwatch/static/js/hancon.js
@@ -1,7 +1,10 @@
 var hancon = {
-    dwell : "175",
+    // in ms
+    dwell : "150",
     pause : "2000",
-    auto_refresh : "1",
+
+    // in minutes
+    auto_refresh : "5",
 
     image_base : "http://dctsleeperservice:9876/goes16/nows/",
     basename : "goes_latest_???.png",
@@ -10,14 +13,13 @@ var hancon = {
 
     window_size : "div",
 
-    // filenames: "goes_latest_023.png, goes_latest_024.png, goes_latest_025.png, goes_latest_026.png, goes_latest_027.png, goes_latest_028.png, goes_latest_029.png, goes_latest_030.png, goes_latest_031.png, goes_latest_032.png, goes_latest_033.png, goes_latest_034.png, goes_latest_035.png, goes_latest_036.png, goes_latest_037.png, goes_latest_038.png, goes_latest_039.png, goes_latest_040.png, goes_latest_041.png, goes_latest_042.png, goes_latest_043.png, goes_latest_044.png, goes_latest_045.png, goes_latest_046.png, goes_latest_047.png",
     // NOTE: For background images that are changing on the server, but
     //   have the same filename, you may specify the parameter
     background_static : "n",
 
     controls : "startstop,speed,step,refresh,looprock,toggle",
     controls_style : "padding:10px; display:flex;",
-    controls_tooltip : "Start/Stop, Speed Control, Single Step , Refresh, Rock Forwards/Backwards,",
+    controls_tooltip : "Start/Stop, Speed Control, Single Step, Refresh, Rock Forwards/Backwards,",
 
     buttons_style : "flex:auto; margin:3px;",
 
