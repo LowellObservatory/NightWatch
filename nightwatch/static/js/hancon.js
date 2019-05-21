54b1e1bf92a7a129109a7c1f4154b40d596f5d0b
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-05-21 16:07:07 -0700

    Enable HAniS mousewheel zooming
    Also keep the top overlay static to see timestamp

--- nightwatch/static/js/hancon.js
+++ nightwatch/static/js/hancon.js
@@ -1,28 +1,34 @@
 var hancon = {
     // in ms
-    dwell : "150",
-    pause : "2000",
+    dwell: "150",
+    pause: "2000",
 
     // in minutes
-    auto_refresh : "5",
+    auto_refresh: "5",
 
-    image_base : "http://dctsleeperservice:9876/goes16/nows/",
-    basename : "goes_latest_???.png",
-    num_frames : "24",
+    image_base: "http://dctsleeperservice:9876/goes16/nows/",
+    basename: "goes_latest_???.png",
+    num_frames: "24",
     base_starting_number: "23",
 
-    window_size : "div",
+    window_size: "div",
 
     // NOTE: For background images that are changing on the server, but
     //   have the same filename, you may specify the parameter
-    background_static : "n",
+    background_static: "n",
 
-    controls : "startstop,speed,step,refresh,looprock,toggle",
-    controls_style : "padding:10px; display:flex;",
-    controls_tooltip : "Start/Stop, Speed Control, Single Step, Refresh, Rock Forwards/Backwards,",
+    active_zoom: "true",
+    maximum_zoom: "2.5",
+    image_preserve: "0, 0, 700, 31",
+    hide_top: "31, true",
+    overlay_zoom: "n",
 
-    buttons_style : "flex:auto; margin:3px;",
+    controls: "startstop,speed,step,refresh,looprock,toggle",
+    controls_style: "padding:10px; display:flex;",
+    controls_tooltip: "Start/Stop, Speed Control, Single Step, Refresh, Rock Forwards/Backwards",
 
-    toggle_size : "20, 10, 5",
-    toggle_colors : "#005914, red, orange",
+    buttons_style: "flex:auto; margin:3px;",
+
+    toggle_size: "20, 10, 5",
+    toggle_colors: "#005914, red, orange",
   };
