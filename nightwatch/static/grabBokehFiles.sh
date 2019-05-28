f4350ff5f32ff711f5b706bd7ee18f6933a061d6
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-05-28 16:21:35 -0700

    Update to bokeh 1.2.0 staticfiles
    Also include helpful grabber utility

---
+++ nightwatch/static/grabBokehFiles.sh
@@ -0,0 +1,13 @@
+ver=1.2.0
+
+# CSS Files
+wget -O "css/bokeh.min.css" "http://cdn.pydata.org/bokeh/release/bokeh-"$ver".min.css"
+wget -O "css/bokeh-tables.min.css" "http://cdn.pydata.org/bokeh/release/bokeh-tables-"$ver".min.css"
+wget -O "css/bokeh-widgets.min.css" "http://cdn.pydata.org/bokeh/release/bokeh-widgets-"$ver".min.css"
+
+# JS Files
+wget -O "js/bokeh.min.js" "http://cdn.pydata.org/bokeh/release/bokeh-"$ver".min.js"
+wget -O "js/bokeh-api.min.js" "http://cdn.pydata.org/bokeh/release/bokeh-api-"$ver".min.js"
+wget -O "js/bokeh-gl.min.js" "http://cdn.pydata.org/bokeh/release/bokeh-gl-"$ver".min.js"
+wget -O "js/bokeh-tables.min.js" "http://cdn.pydata.org/bokeh/release/bokeh-tables-"$ver".min.js"
+wget -O "js/bokeh-widgets.min.js" "http://cdn.pydata.org/bokeh/release/bokeh-widgets-"$ver".min.js"
